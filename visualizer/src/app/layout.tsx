155e148c92a6fd2f854fb1a567dff435054ef74d
Avirup <avirupds11@gmail.com>
2026-03-11 00:14:53 +0530

    fix(hydration): Move Google Analytics strictly inside body block to prevent fatal Hydration mismatch

--- visualizer/src/app/layout.tsx
+++ visualizer/src/app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoogleAnalytics gaId="G-1JS0719YBT" />
       </body>
-      <GoogleAnalytics gaId="G-1JS0719YBT" />
     </html>
   );
 }
